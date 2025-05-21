18c36b6ef611a77603da57268e2d0af8ea2cb89b
Arttero <artt3ro@gmail.com>
2025-05-21 12:38:08 +0200

    test

--- src/CheckTask.java
+++ src/CheckTask.java
@@ -29,11 +29,11 @@ public class CheckTask extends JFrame {
                     price += 3500;
                 }
                 if (cCheckBox.isSelected()) {
-                    message += "C# price 3 000 PLN\n";
+                    message += "C# #price 3 000 PLN\n";
                     price += 3000;
                 }
                 if (cppCheckBox.isSelected()) {
-                    message += "C# price 4 000 PLN\n";
+                    message += "C++ #price 4 000 PLN\n";
                     price += 4000;
                 }
                 if (pythonCheckBox.isSelected()) {
@@ -41,7 +41,7 @@ public class CheckTask extends JFrame {
                     price += 5000;
                 }
                 message += "-------------------------------\n";
-                JOptionPane.showMessageDialog(null, message+ " Razem: " + price + "PLN");
+                JOptionPane.showMessageDialog(null, message+ " Razem: " + price + " PLN");
             }
         });
     }
